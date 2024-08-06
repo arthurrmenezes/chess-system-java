59bc6a95faccb2d0c1b2f2c61c4285c866cde73b
Arthur Menezes <arthur.menezes2003@hotmail.com>
2024-08-06 11:34:36 -0300

    Chess layer and printing board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+import chess.ChessMatch;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printboard(chessMatch.getPieces());
+
     }
 }
